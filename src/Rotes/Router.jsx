647942b97ffaa38b3c9684a7e3ec8e7be6687b4a
JP Choyon Khan <jpchoyonkhan65@gmail.com>
2023-11-04 23:18:22 +0600

    router setup and navbar section done

---
+++ src/Rotes/Router.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+
+
+const Router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        
+      }
+    ]
+  }
+])
+
+export default Router;
